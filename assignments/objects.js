e207bea010c0d1d2d9cf8a6157f9208cc68cad6c
wktg623 <wktg623@gmail.com>
2019-10-01 21:22:53 -0500

    done with stretch

--- assignments/objects.js
+++ assignments/objects.js
@@ -95,16 +95,51 @@ console.log(intern5.firstName + " can multiply two numbers together as evidenced
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name: "Susan",
+
+  age: 70,
+
+child: 
+  {name: "George",
+
+age: 50,
+
+grandchild:
+  {name: "Sam",
+
+age: 30,
+
+speaks: function(){
+  return this.name + " speaks!"}
+
+
+}
+
+, 
+speaks: function(){
+  return this.name + " speaks!"}
+
+}
+
+,
+speaks: function(){
+  return this.name + " speaks!"}
+
+
+}
+
 
 // Log the parent object's name
-
+console.log(parent.name);
 // Log the child's age
-
+console.log(parent.child.age);
 // Log the name and age of the grandchild
+console.log(parent.child.grandchild)
 
 // Have the parent speak
-
+console.log(parent.speaks());
 // Have the child speak
-
+console.log(parent.child.speaks());
 // Have the grandchild speak
+console.log(parent.child.grandchild.speaks());
